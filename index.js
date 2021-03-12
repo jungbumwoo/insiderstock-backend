ed7f0c443d17bc611589390a84dbcb9ffc51e156
jungbumwoo <jungbumwoo@naver.com>
2021-03-12 14:37:59 +0900

    success: get all td and innerText

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 import axios from "axios";
 import puppeteer from "puppeteer";
+import e from "express";
 
 // import "./src/db";
 dotenv.config();
@@ -31,13 +32,31 @@ puppeteer
 
         const tbodyTag = '#wrapper > div > table > tbody';
         const trTag = '#wrapper > div > table > tbody > tr';
+        const tdTag = '#wrapper > div > table > tbody > tr > td'
         await page.waitForSelector(trTag);
 
        
         const isthere = await page.$$eval(trTag, trs => {
             let bucket = [];
             trs.forEach(tr => {
-                bucket.push(tr.innerHTML);
+                    // bucket.push(tr.innerHTML);
+                    let trTds = tr.querySelectorAll('td');
+                    let trBucket = [];
+                    trTds.forEach(td => {
+                        let text;
+                        text = td.innerText;
+                        // let innerSpan = td.querySelector('span');
+                        // let innerDiv = td.querySelector('div');
+                        // if (innerSpan) {
+                        //     text = innerSpan.innerText;
+                        // } else if(innerDiv) {
+                        //     text = innerDiv.innerText;
+                        // } else {
+                        //     text = "muyaho"
+                        // }
+                        trBucket.push(text);
+                    })
+                    bucket.push(trBucket);
                 })
             return bucket; 
             }
