f55c186eb215b0d5f791cdeeace46e3f8a66aba8
jungbumwoo <jungbumwoo@naver.com>
2021-03-13 12:09:12 +0900

    trying to get next page

--- src/controllers/stockController.js
+++ src/controllers/stockController.js
@@ -1,7 +1,11 @@
 import puppeteer from "puppeteer";
 
+// 첫 페이지 뜨는 거 읽은 다음에 다음 페이지는 상황을 봐가며 읽던가 멈추던가 하는 방법이 있고
+// 아에 처음부터 페이지를 돌리는데 상황을 보고 멈추는 방법도 있고.
+
+
+
 export const stock = (req, res) => {
-    console.log("stock controller");
     puppeteer
     .launch()
     .then(async browser => {
@@ -12,12 +16,56 @@ export const stock = (req, res) => {
         await page.waitForSelector(selector);
         await page.click(selector);
 
-        const tbodyTag = '#wrapper > div > table > tbody';
         const trTag = '#wrapper > div > table > tbody > tr';
-        const tdTag = '#wrapper > div > table > tbody > tr > td'
+        const activePageTag = '#components-root > div > div.insider-page > div:nth-child(9) > div > ul > li.number.active';
         await page.waitForSelector(trTag);
 
-        const isthere = await page.$$eval(trTag, trs => {
+        let pageNum = await page.$eval(activePageTag, num => num.innerText);
+        const mainpage = await page.$$eval(trTag, trs => {
+            let bucket = [];
+            trs.forEach(tr => {
+                    // bucket.push(tr.innerHTML);
+                    let trTds = tr.querySelectorAll('td');
+                    let trBucket = [];
+                    trTds.forEach(td => {
+                        let text;
+                        text = td.innerText;
+                        // let innerSpan = td.querySelector('span');
+                        // let innerDiv = td.querySelector('div');
+                        // if (innerSpan) {
+                        //     text = innerSpan.innerText;
+                        // } else if(innerDiv) {
+                        //     text = innerDiv.innerText;
+                        // } else {
+                        //     text = "muyaho"
+                        // }
+                        trBucket.push(text);
+                    })
+                    bucket.push(trBucket);
+                })
+            return bucket;
+            }
+        );
+        let nextpagelist = nextpage(pageNum);
+        console.log(nextpagelist);
+        return res.status(200).json({mainpage}); 
+    })
+}
+
+const nextpage = async (pageNumber, waitsecond = 500) => {
+    let newpageNum = parseInt(pageNumber) + 1;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.gurufocus.com/insider/summary');
+
+    const trTag = '#wrapper > div > table > tbody > tr';
+    await page.waitForSelector(trTag);
+
+    await page.click(`<li class="number">${newpageNum}</li>`);
+    await page.waitForTimeout(waitsecond);
+    let isLoading = await page.$('nuxt-progress');
+    if (!isLoading) {
+        const anotherPage = await page.$$eval(trTag, trs => {
             let bucket = [];
             trs.forEach(tr => {
                     // bucket.push(tr.innerHTML);
@@ -42,6 +90,9 @@ export const stock = (req, res) => {
             return bucket; 
             }
         );
-        return res.status(200).json({isthere});
-    })
+        return anotherPage;
+    } else {
+        nextpage(pageNumber, waitsecond + 1000);
+    }   
 }
+
