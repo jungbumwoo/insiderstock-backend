651a1e922dff305d4e394b83d9a50eed651b23bb
jungbumwoo <jungbumwoo@naver.com>
2021-03-21 18:58:33 +0900

    post data from frontend

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-express.json();
+// express.json();
+// express.urlencoded({ extended: true })
 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use('/api', stockRouter);
