3c8a90e8563c01bc58493d5a73fd3df2ca0d4738
jungbumwoo <jungbumwoo@naver.com>
2021-03-25 19:29:45 +0900

    Yeah. success to redirect with token url

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,14 +11,9 @@ export const postLogin = (req, res) => {
 export const getToken = (req, res, next) => {
     if(true) {
         // signin
-        console.log("req.session at getToken Func.");
-        console.log(req.session);
         const { accessToken } = req.session.passport.user;
-        
         console.log(accessToken);
-        console.log("req")
-        console.log("getToken Func");
-        return res.redirect(`localhost:3000/user/${accessToken}`)
+        return res.redirect(`http://localhost:3000/${accessToken}/#`)
 
     } else {
         //signup
